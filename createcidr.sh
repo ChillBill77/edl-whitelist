14ff41e4f0c9276ec2b7cb53ae809d7839566404
ChillBill77 <129118422+ChillBill77@users.noreply.github.com>
2024-12-31 10:48:40 +0100

    Update createcidr.sh

--- createcidr.sh
+++ createcidr.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-mkdir -p 
+mkdir -p lists
 curl -s -H "Accept: application/json" https://www.gstatic.com/ipranges/goog.json | jq -r '.prefixes[].ipv4Prefix | select (. != null)' >./lists/GoogleIPv4.cidr
 curl -s -H "Accept: application/json" https://www.gstatic.com/ipranges/goog.json | jq -r '.prefixes[].ipv6Prefix | select (. != null)' >./lists/GoogleIPv6.cidr
 curl -s -H "Accept: application/json" https://www.gstatic.com/ipranges/cloud.json | jq -r '.prefixes[].ipv4Prefix | select (. != null)' >>./lists/GoogleIPv4.cidr
