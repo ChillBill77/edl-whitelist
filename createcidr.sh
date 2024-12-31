14a0d582e3f5e521cb26599a1d193e6269746aab
ChillBill77 <129118422+ChillBill77@users.noreply.github.com>
2024-12-31 10:10:18 +0100

    Update createcidr.sh

--- createcidr.sh
+++ createcidr.sh
@@ -1,7 +1,7 @@
 #/bin/bash
-curl -s -H "Accept: application/json" https://www.gstatic.com/ipranges/goog.json | jq -r '.prefixes[].ipv4Prefix | select (. != null)' >./GoogleIPv4.cidr
-curl -s -H "Accept: application/json" https://www.gstatic.com/ipranges/goog.json | jq -r '.prefixes[].ipv6Prefix | select (. != null)' >./GoogleIPv6.cidr
-curl -s -H "Accept: application/json" https://www.gstatic.com/ipranges/cloud.json | jq -r '.prefixes[].ipv4Prefix | select (. != null)' >>./GoogleIPv4.cidr
-curl -s -H "Accept: application/json" https://www.gstatic.com/ipranges/cloud.json | jq -r '.prefixes[].ipv6Prefix | select (. != null)' >>./GoogleIPv6.cidr
-curl -s -H "Accept: application/json" "https://endpoints.office.com/endpoints/worldwide?clientrequestid=b10c5ed1-bad1-445f-b386-b919946339a7" | jq -r '.[] | select(.ips) |  .ips[]' > MicrosoftIP.cidr
-curl -s -H "Accept: application/json" "https://endpoints.office.com/endpoints/worldwide?clientrequestid=b10c5ed1-bad1-445f-b386-b919946339a7" | jq -r '.[] | select(.urls) |  .urls[]' >Microsoft.urls
+curl -s -H "Accept: application/json" https://www.gstatic.com/ipranges/goog.json | jq -r '.prefixes[].ipv4Prefix | select (. != null)' >./lists/GoogleIPv4.cidr
+curl -s -H "Accept: application/json" https://www.gstatic.com/ipranges/goog.json | jq -r '.prefixes[].ipv6Prefix | select (. != null)' >./lists/GoogleIPv6.cidr
+curl -s -H "Accept: application/json" https://www.gstatic.com/ipranges/cloud.json | jq -r '.prefixes[].ipv4Prefix | select (. != null)' >>./lists/GoogleIPv4.cidr
+curl -s -H "Accept: application/json" https://www.gstatic.com/ipranges/cloud.json | jq -r '.prefixes[].ipv6Prefix | select (. != null)' >>./lists/GoogleIPv6.cidr
+curl -s -H "Accept: application/json" "https://endpoints.office.com/endpoints/worldwide?clientrequestid=b10c5ed1-bad1-445f-b386-b919946339a7" | jq -r '.[] | select(.ips) |  .ips[]' > ./lists/MicrosoftIP.cidr
+curl -s -H "Accept: application/json" "https://endpoints.office.com/endpoints/worldwide?clientrequestid=b10c5ed1-bad1-445f-b386-b919946339a7" | jq -r '.[] | select(.urls) |  .urls[]' > ./lists/Microsoft.urls
